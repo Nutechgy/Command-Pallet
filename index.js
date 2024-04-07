a71d7fcc808580b1a49e98c6578e9a1217481187
Your Name <you@example.com>
2024-04-07 08:50:47 -0400

    restored index

--- index.js
+++ index.js
@@ -33,28 +33,7 @@ async function getUserInputAndGenerateLogo() {
 
     // Initialize svgContent
     let svgContent = '';
-
-    // Generate SVG content
-    if (userInput.shape === 'square') {
-      svgContent = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
-          <rect width="100%" height="100%" fill="${chroma(userInput.shapeColor)}"/>
-          <text x="50" y="50" font-size="30" fill="${chroma(userInput.textColor)}" dominant-baseline="middle" text-anchor="middle">
-        </svg>
-      `;
-    } else if (userInput.shape === 'circle') {
-      svgContent = `2        <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
-          <circle r="45" cx="50" cy="50" fill="${chroma(userInput.shapeColor)}" />
-          <text x="50" y="50" font-size="30" fill="${chroma(userInput.textColor)}" dominant-baseline="middle" text-anchor="middle">${userInput.text}</text>
-        </svg>
-      `;
-    } else {
-      svgContent = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
-      <polygon points="100,10 150,190 50,190" fill="${chroma(userInput.shapeColor)}" />
-      <text x="50" y="50" font-size="30" fill="${chroma(userInput.textColor)}" dominant-baseline="middle" text-anchor="middle">${userInput.text}</text>
-    </svg>
-      `
+baseline="middle" text-anchor="middle">b>baseline="middle" text-anchor="middle">${userInput.text}</text>bine="middle" text-anchor="middle">${userInput.text}</text>b
     }
 
 
